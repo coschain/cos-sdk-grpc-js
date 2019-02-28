f09908aca981461f19aa7d61a9237bbd5c26941c
AprocySanae <axdiaoqi220@gmail.com>
2019-02-28 12:22:35 +0800

    add function into prototype

--- src/lib/type/account.ts
+++ src/lib/type/account.ts
@@ -1,11 +1,22 @@
-import {account_name as accountName} from "../../prototype/type_pb"
+import {account_name}  from "../../prototype/type_pb"
 
-export class account_name extends accountName {
-    validate = () => {
-        let name = this.getValue();
-        if(name.length < 6 || name.length > 16) {
-            return false
-        }
-        return name.match(/^[A-Za-z0-9]+$/i) !== null;
-    };
-}
+// export class account_name extends accountName {
+//     validate = () => {
+//         let name = this.getValue();
+//         if(name.length < 6 || name.length > 16) {
+//             return false
+//         }
+//         return name.match(/^[A-Za-z0-9]+$/i) !== null;
+//     };
+// }
+
+// @ts-ignore
+account_name.prototype.validate = function (){
+    let name = this.getValue();
+    if(name.length < 6 || name.length > 16) {
+        return false
+    }
+    return name.match(/^[A-Za-z0-9]+$/i) !== null;
+};
+
+export {account_name};
